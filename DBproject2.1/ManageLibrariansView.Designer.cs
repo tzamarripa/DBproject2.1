f58aed8b4fb2757610b6d41b4a29febe5fa9703e
Richard Ogin <rogin@users.noreply.github.com>
2019-11-13 22:17:13 -0600

    UI cleanup for Manage Librarians view

--- DBproject2.1/ManageLibrariansView.Designer.cs
+++ DBproject2.1/ManageLibrariansView.Designer.cs
@@ -29,8 +29,6 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.lblCount = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.gridSearchResults = new System.Windows.Forms.DataGridView();
             this.btnSearchByName = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
@@ -44,31 +42,18 @@
             this.errorId = new System.Windows.Forms.ErrorProvider(this.components);
             this.errorFirstname = new System.Windows.Forms.ErrorProvider(this.components);
             this.errorLastname = new System.Windows.Forms.ErrorProvider(this.components);
+            this.groupSearchResults = new System.Windows.Forms.GroupBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupIdSearch = new System.Windows.Forms.GroupBox();
+            this.groupNameSearch = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.gridSearchResults)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.errorId)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.errorFirstname)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.errorLastname)).BeginInit();
+            this.groupSearchResults.SuspendLayout();
+            this.groupIdSearch.SuspendLayout();
+            this.groupNameSearch.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // lblCount
-            // 
-            this.lblCount.AutoSize = true;
-            this.lblCount.Font = new System.Drawing.Font("MS UI Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.lblCount.Location = new System.Drawing.Point(265, 301);
-            this.lblCount.Name = "lblCount";
-            this.lblCount.Size = new System.Drawing.Size(19, 19);
-            this.lblCount.TabIndex = 35;
-            this.lblCount.Text = "0";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label4.Location = new System.Drawing.Point(123, 301);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(137, 19);
-            this.label4.TabIndex = 34;
-            this.label4.Text = "Search Results:";
             // 
             // gridSearchResults
             // 
@@ -77,20 +62,20 @@
             this.gridSearchResults.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.gridSearchResults.CausesValidation = false;
             this.gridSearchResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.gridSearchResults.Location = new System.Drawing.Point(118, 351);
+            this.gridSearchResults.Location = new System.Drawing.Point(0, 20);
             this.gridSearchResults.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.gridSearchResults.MultiSelect = false;
             this.gridSearchResults.Name = "gridSearchResults";
             this.gridSearchResults.ReadOnly = true;
             this.gridSearchResults.RowHeadersWidth = 62;
             this.gridSearchResults.RowTemplate.Height = 27;
-            this.gridSearchResults.Size = new System.Drawing.Size(779, 177);
+            this.gridSearchResults.Size = new System.Drawing.Size(1043, 218);
             this.gridSearchResults.TabIndex = 33;
             // 
             // btnSearchByName
             // 
             this.btnSearchByName.Font = new System.Drawing.Font("MS UI Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.btnSearchByName.Location = new System.Drawing.Point(729, 198);
+            this.btnSearchByName.Location = new System.Drawing.Point(306, 20);
             this.btnSearchByName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSearchByName.Name = "btnSearchByName";
             this.btnSearchByName.Size = new System.Drawing.Size(168, 63);
@@ -103,7 +88,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("MS UI Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label3.Location = new System.Drawing.Point(386, 120);
+            this.label3.Location = new System.Drawing.Point(355, 118);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(66, 19);
             this.label3.TabIndex = 31;
@@ -113,7 +98,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("MS UI Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label2.Location = new System.Drawing.Point(258, 242);
+            this.label2.Location = new System.Drawing.Point(21, 59);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(87, 19);
             this.label2.TabIndex = 30;
@@ -121,25 +106,25 @@
             // 
             // txtLastname
             // 
-            this.txtLastname.Location = new System.Drawing.Point(449, 242);
+            this.txtLastname.Location = new System.Drawing.Point(116, 58);
             this.txtLastname.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtLastname.Name = "txtLastname";
-            this.txtLastname.Size = new System.Drawing.Size(128, 22);
+            this.txtLastname.Size = new System.Drawing.Size(149, 22);
             this.txtLastname.TabIndex = 29;
             // 
             // txtFirstname
             // 
-            this.txtFirstname.Location = new System.Drawing.Point(449, 186);
+            this.txtFirstname.Location = new System.Drawing.Point(116, 20);
             this.txtFirstname.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtFirstname.Name = "txtFirstname";
-            this.txtFirstname.Size = new System.Drawing.Size(128, 22);
+            this.txtFirstname.Size = new System.Drawing.Size(149, 22);
             this.txtFirstname.TabIndex = 28;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("MS UI Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(258, 189);
+            this.label1.Location = new System.Drawing.Point(21, 18);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(89, 19);
             this.label1.TabIndex = 27;
@@ -147,17 +132,17 @@
             // 
             // txtId
             // 
-            this.txtId.Location = new System.Drawing.Point(449, 40);
+            this.txtId.Location = new System.Drawing.Point(116, 40);
             this.txtId.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtId.Name = "txtId";
-            this.txtId.Size = new System.Drawing.Size(128, 22);
+            this.txtId.Size = new System.Drawing.Size(149, 22);
             this.txtId.TabIndex = 26;
             // 
             // lblID
             // 
             this.lblID.AutoSize = true;
             this.lblID.Font = new System.Drawing.Font("MS UI Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.lblID.Location = new System.Drawing.Point(258, 43);
+            this.lblID.Location = new System.Drawing.Point(55, 42);
             this.lblID.Name = "lblID";
             this.lblID.Size = new System.Drawing.Size(26, 19);
             this.lblID.TabIndex = 25;
@@ -166,7 +151,7 @@
             // btnSearchById
             // 
             this.btnSearchById.Font = new System.Drawing.Font("MS UI Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.btnSearchById.Location = new System.Drawing.Point(729, 53);
+            this.btnSearchById.Location = new System.Drawing.Point(306, 20);
             this.btnSearchById.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSearchById.Name = "btnSearchById";
             this.btnSearchById.Size = new System.Drawing.Size(168, 63);
@@ -187,23 +172,48 @@
             // 
             this.errorLastname.ContainerControl = this;
             // 
+            // groupSearchResults
+            // 
+            this.groupSearchResults.Controls.Add(this.gridSearchResults);
+            this.groupSearchResults.Location = new System.Drawing.Point(12, 275);
+            this.groupSearchResults.Name = "groupSearchResults";
+            this.groupSearchResults.Size = new System.Drawing.Size(1043, 277);
+            this.groupSearchResults.TabIndex = 36;
+            this.groupSearchResults.TabStop = false;
+            // 
+            // groupIdSearch
+            // 
+            this.groupIdSearch.Controls.Add(this.txtId);
+            this.groupIdSearch.Controls.Add(this.lblID);
+            this.groupIdSearch.Controls.Add(this.btnSearchById);
+            this.groupIdSearch.Location = new System.Drawing.Point(285, 12);
+            this.groupIdSearch.Name = "groupIdSearch";
+            this.groupIdSearch.Size = new System.Drawing.Size(480, 92);
+            this.groupIdSearch.TabIndex = 37;
+            this.groupIdSearch.TabStop = false;
+            // 
+            // groupNameSearch
+            // 
+            this.groupNameSearch.Controls.Add(this.label1);
+            this.groupNameSearch.Controls.Add(this.txtFirstname);
+            this.groupNameSearch.Controls.Add(this.txtLastname);
+            this.groupNameSearch.Controls.Add(this.btnSearchByName);
+            this.groupNameSearch.Controls.Add(this.label2);
+            this.groupNameSearch.Location = new System.Drawing.Point(285, 149);
+            this.groupNameSearch.Name = "groupNameSearch";
+            this.groupNameSearch.Size = new System.Drawing.Size(480, 100);
+            this.groupNameSearch.TabIndex = 38;
+            this.groupNameSearch.TabStop = false;
+            // 
             // ManageLibrariansView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1067, 554);
-            this.Controls.Add(this.lblCount);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.gridSearchResults);
-            this.Controls.Add(this.btnSearchByName);
+            this.Controls.Add(this.groupNameSearch);
+            this.Controls.Add(this.groupIdSearch);
+            this.Controls.Add(this.groupSearchResults);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtLastname);
-            this.Controls.Add(this.txtFirstname);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtId);
-            this.Controls.Add(this.lblID);
-            this.Controls.Add(this.btnSearchById);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "ManageLibrariansView";
             this.Text = "Manage Librarians";
@@ -212,15 +222,17 @@
             ((System.ComponentModel.ISupportInitialize)(this.errorId)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.errorFirstname)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.errorLastname)).EndInit();
+            this.groupSearchResults.ResumeLayout(false);
+            this.groupIdSearch.ResumeLayout(false);
+            this.groupIdSearch.PerformLayout();
+            this.groupNameSearch.ResumeLayout(false);
+            this.groupNameSearch.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label lblCount;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.DataGridView gridSearchResults;
         private System.Windows.Forms.Button btnSearchByName;
         private System.Windows.Forms.Label label3;
@@ -234,5 +246,9 @@
         private System.Windows.Forms.ErrorProvider errorId;
         private System.Windows.Forms.ErrorProvider errorFirstname;
         private System.Windows.Forms.ErrorProvider errorLastname;
+        private System.Windows.Forms.GroupBox groupSearchResults;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.GroupBox groupNameSearch;
+        private System.Windows.Forms.GroupBox groupIdSearch;
     }
 }
