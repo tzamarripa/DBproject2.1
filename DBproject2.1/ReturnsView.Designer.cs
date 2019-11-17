947e9be5116e78bbf0829d88fb9e55e967f4a0bb
Richard Ogin <rogin@users.noreply.github.com>
2019-11-17 17:15:47 -0600

    Implement Returns view

--- DBproject2.1/ReturnsView.Designer.cs
+++ DBproject2.1/ReturnsView.Designer.cs
@@ -28,19 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.btnReceive = new System.Windows.Forms.Button();
+            this.txtBarcode = new System.Windows.Forms.TextBox();
+            this.lblBarcode = new System.Windows.Forms.Label();
+            this.errorBarcode = new System.Windows.Forms.ErrorProvider(this.components);
+            this.groupLog = new System.Windows.Forms.GroupBox();
+            this.richLog = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.errorBarcode)).BeginInit();
+            this.groupLog.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // btnReceive
+            // 
+            this.btnReceive.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReceive.Location = new System.Drawing.Point(565, 69);
+            this.btnReceive.Name = "btnReceive";
+            this.btnReceive.Size = new System.Drawing.Size(173, 82);
+            this.btnReceive.TabIndex = 2;
+            this.btnReceive.Text = "Receive";
+            this.btnReceive.UseVisualStyleBackColor = true;
+            this.btnReceive.Click += new System.EventHandler(this.btnReceive_Click);
+            // 
+            // txtBarcode
+            // 
+            this.txtBarcode.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtBarcode.Location = new System.Drawing.Point(373, 105);
+            this.txtBarcode.Name = "txtBarcode";
+            this.txtBarcode.Size = new System.Drawing.Size(174, 22);
+            this.txtBarcode.TabIndex = 1;
+            // 
+            // lblBarcode
+            // 
+            this.lblBarcode.AutoSize = true;
+            this.lblBarcode.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBarcode.Location = new System.Drawing.Point(227, 94);
+            this.lblBarcode.Name = "lblBarcode";
+            this.lblBarcode.Size = new System.Drawing.Size(126, 36);
+            this.lblBarcode.TabIndex = 3;
+            this.lblBarcode.Text = "Barcode";
+            // 
+            // errorBarcode
+            // 
+            this.errorBarcode.ContainerControl = this;
+            // 
+            // groupLog
+            // 
+            this.groupLog.Controls.Add(this.richLog);
+            this.groupLog.Location = new System.Drawing.Point(100, 175);
+            this.groupLog.Name = "groupLog";
+            this.groupLog.Size = new System.Drawing.Size(852, 334);
+            this.groupLog.TabIndex = 4;
+            this.groupLog.TabStop = false;
+            this.groupLog.Text = "Activity Log";
+            // 
+            // richLog
+            // 
+            this.richLog.CausesValidation = false;
+            this.richLog.DetectUrls = false;
+            this.richLog.Location = new System.Drawing.Point(7, 22);
+            this.richLog.Name = "richLog";
+            this.richLog.ReadOnly = true;
+            this.richLog.Size = new System.Drawing.Size(839, 306);
+            this.richLog.TabIndex = 0;
+            this.richLog.Text = "";
+            this.richLog.WordWrap = false;
             // 
             // ReturnsView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.groupLog);
+            this.Controls.Add(this.lblBarcode);
+            this.Controls.Add(this.txtBarcode);
+            this.Controls.Add(this.btnReceive);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ReturnsView";
             this.Text = "Process Returns";
+            this.Load += new System.EventHandler(this.ReturnsView_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorBarcode)).EndInit();
+            this.groupLog.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnReceive;
+        private System.Windows.Forms.TextBox txtBarcode;
+        private System.Windows.Forms.Label lblBarcode;
+        private System.Windows.Forms.ErrorProvider errorBarcode;
+        private System.Windows.Forms.GroupBox groupLog;
+        private System.Windows.Forms.RichTextBox richLog;
     }
 }
--- DBproject2.1/ReturnsView.cs
+++ DBproject2.1/ReturnsView.cs
@@ -29,5 +29,144 @@ namespace DBproject2._1
 
             InitializeComponent();
         }
+
+        private void ReturnsView_Load(object sender, EventArgs e)
+        {
+            SetFocusToBarcodeInput();
+        }
+
+        private void SetFocusToBarcodeInput()
+        {
+            txtBarcode.Focus();
+        }
+
+        private void btnReceive_Click(object sender, EventArgs e)
+        {
+            ClearErrorErrors();
+
+            if (ValidateInput())
+            {
+                var matchingCheckouts = GetCurrentCheckoutsByBarcode();
+
+                if(matchingCheckouts.Count == 0)
+                {
+                    MessageBox.Show("No checkouts contain that barcode", "No matches", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    
+                    //select the barcode text and give it focus
+                    txtBarcode.Select(0, txtBarcode.TextLength);
+                    txtBarcode.Focus();
+                } else
+                {
+                    //if more than one, give to oldest checkout
+                    var matchingCheckout = matchingCheckouts[0];
+
+                    DateTime receptionTime = DateTime.Now;
+                    
+                    if(ReceiveItem(matchingCheckout, receptionTime))
+                    {
+                        LogReceive(matchingCheckout, receptionTime);
+                        ClearInputs();
+                        SetFocusToBarcodeInput();
+                    } else
+                    {
+                        MessageBox.Show("Failed to update database, please try again.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void LogReceive(CheckoutMatch item, DateTime receptionTime)
+        {
+            var message = string.Format("[{0:dd/MM/yyyy hh:mm:ss}] Received barcode '{1}' for member #{2}, initial check out was {3:dd/MM/yyyy}.\n", receptionTime, item.InventoryId, item.MemberId, item.CheckoutDate);
+            richLog.AppendText(message);
+            //ensure the latest message is viewable
+            richLog.ScrollToCaret();
+        }
+
+        private bool ReceiveItem(CheckoutMatch item, DateTime returnTime)
+        {
+            int rowsAffected = 0;
+
+            using (var cmd = DbConnection.CreateCommand())
+            {
+                cmd.CommandText = "update CHECKOUT_ITEM " +
+                                    "set ReturnedDate = @ReturnDate " +
+                                    "where CheckoutID = @CheckoutID " +
+                                    "and InventoryID = @InventoryID " +
+                                    "and ReturnedDate is null";
+
+                cmd.Parameters.AddWithValue("ReturnDate", returnTime);
+                cmd.Parameters.AddWithValue("CheckoutID", item.CheckoutId);
+                cmd.Parameters.AddWithValue("InventoryID", item.InventoryId);
+
+                rowsAffected = cmd.ExecuteNonQuery();
+            }
+
+            return 1 == rowsAffected;
+        }
+
+        private List<CheckoutMatch> GetCurrentCheckoutsByBarcode()
+        {
+            List<CheckoutMatch> results = new List<CheckoutMatch>();
+
+            using (var cmd = DbConnection.CreateCommand())
+            {
+                cmd.CommandText = "select c.CheckoutDate, c.MemberID, ci.CheckoutID, ci.InventoryID " +
+                                    "from checkout c " +
+                                    "join checkout_item ci on ci.CheckoutID = c.CheckoutID " +
+                                    "where ReturnedDate is null " +
+                                    "and ci.InventoryID = @barcode " +
+                                    "order by c.CheckoutDate asc, c.MemberID asc";
+
+                cmd.Parameters.AddWithValue("barcode", txtBarcode.Text);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        results.Add(new CheckoutMatch()
+                        {
+                            CheckoutDate = reader.GetDateTime(0),
+                            MemberId = reader.GetInt32(1),
+                            CheckoutId = reader.GetInt32(2),
+                            InventoryId = reader.GetString(3)
+                        });
+                    }
+                }
+            }
+
+            return results;
+        }
+
+        private bool ValidateInput()
+        {
+            bool valid = true;
+
+            if(txtBarcode.TextLength == 0)
+            {
+                errorBarcode.SetError(txtBarcode, "value required");
+                valid = false;
+            }
+
+            return valid;
+        }
+
+        private void ClearInputs()
+        {
+            txtBarcode.ResetText();
+        }
+
+        private void ClearErrorErrors()
+        {
+            errorBarcode.Clear();
+        }
+
+        private class CheckoutMatch
+        {
+            public DateTime CheckoutDate { get; internal set; }
+            public int MemberId { get; internal set; }
+            public int CheckoutId { get; internal set; }
+            public string InventoryId { get; internal set; }
+        }
     }
 }
